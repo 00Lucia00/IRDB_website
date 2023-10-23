af079d3a3c82a0d4e23d9404e83c50b964c8a900
Lucia <Luciaabouhamad@gmail.com>
2023-10-23 11:32:34 +0200

    updates

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
 // fetch API
-fetch("LuciasAPI-placeholder")
-    .then(response => response.json())//tar ut den rena datan
-    .then((data) => console.log(data));
+fetch("https://localhost:7270/api/Movie/")
+  .then((response) => response.json()) //tar ut den rena datan
+  .then((data) => console.log(data));
